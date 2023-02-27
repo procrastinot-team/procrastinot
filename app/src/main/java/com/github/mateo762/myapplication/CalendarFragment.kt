8eb09bd4a9c1c91a61e390c557dba5e9beb5a8e2
Mateo Echeverry <mateo.echeverryhoyos@gmail.com>
2023-02-27 19:02:19 +0100

    Implement fragments for navigation

---
+++ app/src/main/java/com/github/mateo762/myapplication/CalendarFragment.kt
@@ -0,0 +1,18 @@
+package com.github.mateo762.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CalendarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+}
--- app/src/main/java/com/github/mateo762/myapplication/MainActivity.kt
+++ app/src/main/java/com/github/mateo762/myapplication/MainActivity.kt
@@ -1,21 +1,87 @@
 package com.github.mateo762.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+    lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.mainGreetButton)
-        button.setOnClickListener {
-            val name = findViewById<TextView>(R.id.mainName)
-            val greetIntent = Intent(this@MainActivity, GreetingActivity::class.java)
-            greetIntent.putExtra("name", name.text.toString())
-            this@MainActivity.startActivity(greetIntent)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                CalendarFragment()
+            ).commit()
+            navigationView.setCheckedItem(R.id.nav_calendar)
         }
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_calendar -> {
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.fragment_container,
+                    CalendarFragment()
+                ).commit()
+            }
+            R.id.nav_pictures -> {
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.fragment_container,
+                    PicturesFragment()
+                ).commit()
+            }
+            R.id.nav_profile -> {
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.fragment_container,
+                    ProfileFragment()
+                ).commit()
+            }
+            R.id.nav_settings -> {
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.fragment_container,
+                    SettingsFragment()
+                ).commit()
+            }
+            R.id.nav_share -> {
+                Toast.makeText(this, "Clicked share!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_label -> {
+                Toast.makeText(this, "Clicked label!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
