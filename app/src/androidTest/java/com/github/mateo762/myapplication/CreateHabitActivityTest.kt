e233ce55311e753e37c87bfbac71a961b4a40863
Mateo Echeverry <mateo.echeverryhoyos@gmail.com>
2023-03-08 11:19:27 +0100

    Avoid duplication on CreateHabitActivityTest

--- app/src/androidTest/java/com/github/mateo762/myapplication/CreateHabitActivityTest.kt
+++ app/src/androidTest/java/com/github/mateo762/myapplication/CreateHabitActivityTest.kt
@@ -1,27 +1,15 @@
 package com.github.mateo762.myapplication
 
-import android.os.IBinder
-import android.view.View
-import android.view.WindowManager
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.test.core.app.ActivityScenario.ActivityAction
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Root
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers.*
-import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.mateo762.myapplication.fragments.*
-import org.hamcrest.Description
-import org.hamcrest.Matchers
 import org.hamcrest.Matchers.*
-import org.hamcrest.TypeSafeMatcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -39,230 +27,100 @@ class CreateHabitActivityTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    private lateinit var habitName: String
+    private lateinit var habitDays: List<DayOfWeek>
+    private lateinit var habitStartTime: String
+    private lateinit var habitEndTime: String
 
     @Before
     fun setUp() {
-        // Initialize the Intents framework
+        habitName = "Drink Water"
+        habitDays = listOf(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY, DayOfWeek.FRIDAY)
+        habitStartTime = "07:30"
+        habitEndTime = "08:00"
         Intents.init()
     }
 
     @After
     fun tearDown() {
-        // Release the Intents framework
         Intents.release()
+    }
+
+    private fun createHabit() {
+        // Enter habit name
+        composeTestRule.onNodeWithTag("txt_name")
+            .performTextInput(habitName)
+
+        // Check checkboxes for habit days
+        habitDays.forEach { day ->
+            composeTestRule.onNodeWithTag("checkbox_${day.name}")
+                .performClick()
+        }
+        // Clear start and end time
+        composeTestRule.onNodeWithTag("txt_time_start")
+            .performClick()
+            .performTextClearance()
+        composeTestRule.onNodeWithTag("txt_time_end")
+            .performClick()
+            .performTextClearance()
+
+        // Enter start and end time
+        composeTestRule.onNodeWithTag("txt_time_start")
+            .performTextInput(habitStartTime)
+        composeTestRule.onNodeWithTag("txt_time_end")
+            .performTextInput(habitEndTime)
+
+        // Click save button
+        composeTestRule.onNodeWithTag("btn_save").performClick()
     }
 
     @Test
     fun testCreateHabit() {
-        // Enter habit name
-        composeTestRule.onNodeWithTag("txt_name")
-            .performTextInput("Drink Water")
-
-        // Check checkboxes for Monday and Wednesday
-        composeTestRule.onNodeWithTag("checkbox_MONDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_WEDNESDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_FRIDAY").performClick()
-
-        // Enter start time
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performTextInput("07:30")
-
-        // Enter end time
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performTextInput("08:00")
-
-        // Click save button
-        composeTestRule.onNodeWithTag("btn_save").performClick()
-
-
+        createHabit()
         // Verify that the intent was sent correctly
         intended(
             allOf(
                 hasComponent(DisplayParametersActivity::class.java.name),
-                hasExtra("habitName", "Drink Water"),
-                hasExtra(
-                    "habitDays",
-                    listOf(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY, DayOfWeek.FRIDAY)
-                ),
-                hasExtra("habitStartTime", "07:30"),
-                hasExtra("habitEndTime", "08:00")
+                hasExtra("habitName", habitName),
+                hasExtra("habitDays", habitDays),
+                hasExtra("habitStartTime", habitStartTime),
+                hasExtra("habitEndTime", habitEndTime)
             )
         )
     }
+
     @Test
-    fun clickSaveButton_noNameInput_noIntentSent(){
-        // Check checkboxes for Monday and Wednesday
-        composeTestRule.onNodeWithTag("checkbox_MONDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_WEDNESDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_FRIDAY").performClick()
-
-        // Enter start time
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performTextInput("07:30")
-
-        // Enter end time
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performTextInput("08:00")
-
-        // Click save button
-        composeTestRule.onNodeWithTag("btn_save").performClick()
-
+    fun clickSaveButton_noNameInput_noIntentSent() {
+        habitName = ""
+        createHabit()
         composeTestRule.onNodeWithTag("btn_save").assertIsDisplayed()
     }
 
     @Test
-    fun clickSaveButton_noDaysSelected_noIntentSent(){
-        // Enter habit name
-        composeTestRule.onNodeWithTag("txt_name")
-            .performTextInput("Drink Water")
-
-        // Enter start time
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performTextInput("07:30")
-
-        // Enter end time
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performTextInput("08:00")
-
-        // Click save button
-        composeTestRule.onNodeWithTag("btn_save").performClick()
-
+    fun clickSaveButton_noDaysSelected_noIntentSent() {
+        habitDays = listOf()
+        createHabit()
         composeTestRule.onNodeWithTag("btn_save").assertIsDisplayed()
     }
 
     @Test
-    fun clickSaveButton_noStartTime_noIntentSent(){
-        // Enter habit name
-        composeTestRule.onNodeWithTag("txt_name")
-            .performTextInput("Drink Water")
-
-        // Check checkboxes for Monday and Wednesday
-        composeTestRule.onNodeWithTag("checkbox_MONDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_WEDNESDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_FRIDAY").performClick()
-
-        // Enter end time
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performTextInput("08:00")
-
-
-        // Clear start time
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performClick()
-            .performTextClearance()
-
-        // Click save button
-        composeTestRule.onNodeWithTag("btn_save").performClick()
-
+    fun clickSaveButton_startTimeInvalid_noIntentSent() {
+        habitStartTime = "25:00"
+        createHabit()
         composeTestRule.onNodeWithTag("btn_save").assertIsDisplayed()
     }
+
     @Test
-    fun clickSaveButton_startTimeInvalid_noIntentSent(){
-        // Enter habit name
-        composeTestRule.onNodeWithTag("txt_name")
-            .performTextInput("Drink Water")
-
-        // Check checkboxes for Monday and Wednesday
-        composeTestRule.onNodeWithTag("checkbox_MONDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_WEDNESDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_FRIDAY").performClick()
-
-        // Enter start time
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performTextInput("24:30")
-
-        // Enter end time
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performTextInput("08:00")
-
-        // Click save button
-        composeTestRule.onNodeWithTag("btn_save").performClick()
-
+    fun clickSaveButton_noEndTime_noIntentSent() {
+        habitEndTime = ""
+        createHabit()
         composeTestRule.onNodeWithTag("btn_save").assertIsDisplayed()
     }
+
     @Test
-    fun clickSaveButton_noEndTime_noIntentSent(){
-        // Enter habit name
-        composeTestRule.onNodeWithTag("txt_name")
-            .performTextInput("Drink Water")
-
-        // Check checkboxes for Monday and Wednesday
-        composeTestRule.onNodeWithTag("checkbox_MONDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_WEDNESDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_FRIDAY").performClick()
-
-        // Enter start time
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performTextInput("07:30")
-
-        // Clear end time
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performClick()
-            .performTextClearance()
-
-        // Click save button
-        composeTestRule.onNodeWithTag("btn_save").performClick()
-
-        composeTestRule.onNodeWithTag("btn_save").assertIsDisplayed()
-    }
-    @Test
-    fun clickSaveButton_endTimeInvalid_noIntentSent(){
-        // Enter habit name
-        composeTestRule.onNodeWithTag("txt_name")
-            .performTextInput("Drink Water")
-
-        // Check checkboxes for Monday and Wednesday
-        composeTestRule.onNodeWithTag("checkbox_MONDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_WEDNESDAY").performClick()
-        composeTestRule.onNodeWithTag("checkbox_FRIDAY").performClick()
-
-        // Enter start time
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_start")
-            .performTextInput("07:30")
-
-        // Enter end time
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performClick()
-            .performTextClearance()
-        composeTestRule.onNodeWithTag("txt_time_end")
-            .performTextInput("08:78")
-
-        // Click save button
-        composeTestRule.onNodeWithTag("btn_save").performClick()
-
+    fun clickSaveButton_endTimeInvalid_noIntentSent() {
+        habitEndTime = "08:69"
+        createHabit()
         composeTestRule.onNodeWithTag("btn_save").assertIsDisplayed()
     }
 }
