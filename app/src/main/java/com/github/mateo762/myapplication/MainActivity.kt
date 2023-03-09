e13a188486c73416bec88543debe4cb3eeb0a7bf
Mateo Echeverry <mateo.echeverryhoyos@gmail.com>
2023-03-09 11:40:30 +0100

    Update side menu, remove greet and added habits

--- app/src/main/java/com/github/mateo762/myapplication/MainActivity.kt
+++ app/src/main/java/com/github/mateo762/myapplication/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
                 R.id.fragment_container,
                 HabitsFragment()
             ).commit()
-            navigationView.setCheckedItem(R.id.nav_greet)
+            navigationView.setCheckedItem(R.id.nav_habits)
         }
     }
 
@@ -52,7 +52,7 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_greet -> {
+            R.id.nav_habits -> {
                 openFragmentSelected(HabitsFragment())
             }
             R.id.nav_calendar -> {
