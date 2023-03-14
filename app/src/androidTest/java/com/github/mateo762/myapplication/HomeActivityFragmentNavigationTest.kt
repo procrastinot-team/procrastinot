13f2026a99a969dcd0cb1568c0146409a79c65ae
Jorge Encinas <j.encinasr@alumnos.upm.es>
2023-03-14 17:06:47 +0100

    Restore partial test

--- app/src/androidTest/java/com/github/mateo762/myapplication/HomeActivityFragmentNavigationTest.kt
+++ app/src/androidTest/java/com/github/mateo762/myapplication/HomeActivityFragmentNavigationTest.kt
@@ -16,7 +16,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-// @RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class HomeActivityFragmentNavigationTest {
    private lateinit var activityScenario: ActivityScenario<HomeActivity>
 
