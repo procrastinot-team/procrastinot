9c1a5fbb3d88d5957b5e66bec0e477c03ac4972a
Enrico-Benedettini <enrico.benedettini@gmail.com>
2023-03-28 03:33:30 +0200

    Added user username displayed and profile pic

--- app/src/main/java/com/github/mateo762/myapplication/profile/ProfileActivity.kt
+++ app/src/main/java/com/github/mateo762/myapplication/profile/ProfileActivity.kt
@@ -1,16 +1,24 @@
 package com.github.mateo762.myapplication.profile
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mateo762.myapplication.R
 import com.github.mateo762.myapplication.databinding.ActivityProfileBinding
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.firebase.auth.FirebaseAuth
 
 /**
  * Activity for displaying the profile information.
  */
 class ProfileActivity : AppCompatActivity() {
 
+    private val user = FirebaseAuth.getInstance().currentUser
+    private lateinit var profileImage:ShapeableImageView
     private lateinit var binding: ActivityProfileBinding
     private lateinit var adapter: ProfileGalleryAdapter
 
@@ -25,9 +33,33 @@ class ProfileActivity : AppCompatActivity() {
         adapter.galleryItems = generateTextGalleryItems(R.drawable.ic_new, 13)
         binding.recyclerView.adapter = adapter
 
-        binding.name.text = "John Doe" //todo remove, used for demo and testing
-        binding.username.text = "johndoe12345" //todo remove, used for demo and testing
+        binding.name.text = R.string.missing_name.toString()
+        binding.username.text = user?.email
+
+        profileImage = findViewById<ShapeableImageView>(R.id.profileImage)
+        profileImage.setOnClickListener {
+            val openGalleryIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(openGalleryIntent,1000)
+        }
     }
+
+    @Override
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            if (resultCode == Activity.RESULT_OK) {
+                val imageUri = data?.data
+                profileImage.setImageURI(imageUri)
+//                uploadImageToFirebase(imageUri)
+            }
+        }
+    }
+//
+//    private fun uploadImageToFirebase(imageUri: Uri?) {
+//        val db: DatabaseReference = Firebase.database.reference
+//
+//        val fileRef = db.child("users/${user?.uid}/gravatar")
+//    }
 
     private fun setupToolbar() {
         setSupportActionBar(binding.toolbar)
