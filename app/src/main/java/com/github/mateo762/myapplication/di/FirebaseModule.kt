6ad6ecc37dc25830ffe5fe6f06c7a6bd902d3967
makfazlic <makfazlic@protonmail.com>
2023-03-30 14:04:28 +0200

    Adding hilt

--- app/src/main/java/com/github/mateo762/myapplication/di/FirebaseModule.kt
+++ app/src/main/java/com/github/mateo762/myapplication/di/FirebaseModule.kt
@@ -1,6 +1,8 @@
 package com.github.mateo762.myapplication.di
 
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,6 +17,15 @@ object FirebaseModule {
     @Provides
     @Singleton
     fun provideFirebaseInstance(): FirebaseDatabase{
-        return FirebaseDatabase.getInstance()
+        return Firebase.database
     }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseInstanceEmulated(): FirebaseDatabase{
+        val db = Firebase.database
+        db.useEmulator("10.0.2.2", 9000)
+        return db
+    }
+
 }
