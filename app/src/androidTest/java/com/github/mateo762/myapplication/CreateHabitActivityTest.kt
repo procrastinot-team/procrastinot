9596add00d9574a254928b0793d0b82899036bcd
David Bojkovski <david.bojkovski@epfl.ch>
2023-04-17 16:02:36 +0200

    Fix CreateHabitActivityTest error

--- app/src/androidTest/java/com/github/mateo762/myapplication/CreateHabitActivityTest.kt
+++ app/src/androidTest/java/com/github/mateo762/myapplication/CreateHabitActivityTest.kt
@@ -1,11 +1,7 @@
 package com.github.mateo762.myapplication
 
-import android.widget.TimePicker
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.contrib.PickerActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.*
@@ -25,12 +21,15 @@ import java.time.DayOfWeek
 import java.util.*
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class CreateHabitActivityTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(HabitsActivity::class.java)
 
     @get:Rule
     val composeTestRule = createComposeRule()
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
 
     private lateinit var habitName: String
     private lateinit var habitDays: List<DayOfWeek>
@@ -39,6 +38,7 @@ class CreateHabitActivityTest {
 
     @Before
     fun setUp() {
+        hiltRule.inject()
         habitName = "Drink Water"
         habitDays = listOf(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY, DayOfWeek.FRIDAY)
         habitStartTime = "00:00"
