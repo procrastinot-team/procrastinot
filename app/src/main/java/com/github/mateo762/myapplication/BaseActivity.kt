e351e1dfcb03d862aaa064551b5231380443a14e
makfazlic <makfazlic@protonmail.com>
2023-04-21 12:48:48 +0200

    Fixing some tests which are not a part of this issues 2

--- app/src/main/java/com/github/mateo762/myapplication/BaseActivity.kt
+++ app/src/main/java/com/github/mateo762/myapplication/BaseActivity.kt
@@ -14,7 +14,7 @@ import com.github.mateo762.myapplication.home.HomeActivity
 import com.github.mateo762.myapplication.profile.ProfileActivity
 import com.github.mateo762.myapplication.search.SearchActivity
 import com.github.mateo762.myapplication.settings.SettingsActivity
-import com.github.mateo762.myapplication.upload_gallery.UploadPictureActivity
+//import com.github.mateo762.myapplication.upload_gallery.UploadPictureActivity
 import com.github.mateo762.myapplication.takephoto.TakePhotoActivity
 
 import com.google.android.material.navigation.NavigationView
