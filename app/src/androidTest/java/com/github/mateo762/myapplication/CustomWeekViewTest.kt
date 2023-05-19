d709411e9a1d94ba175acdb71ff08cbab0d1d718
Enrico-Benedettini <enrico.benedettini@gmail.com>
2023-05-19 02:41:03 +0200

    Added trial fix for Custom Week View tests

--- app/src/androidTest/java/com/github/mateo762/myapplication/CustomWeekViewTest.kt
+++ app/src/androidTest/java/com/github/mateo762/myapplication/CustomWeekViewTest.kt
@@ -1,39 +1,83 @@
 import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.view.MotionEvent
-import androidx.test.core.app.ApplicationProvider
 import com.alamkanak.weekview.WeekView
 import com.github.mateo762.myapplication.habits.fragments.week.CustomWeekView
-import org.junit.Assert.assertEquals
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mock
 import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import org.mockito.Mockito.`when`
 
 class CustomWeekViewTest {
+
+    @Mock
+    private lateinit var mockContext: Context
+
+    @Mock
+    private lateinit var mockResources: Resources
+
+
+    @Mock
+    private lateinit var mockConfiguration: Configuration
+
+    @Mock
+    private lateinit var mockAttributeSet: AttributeSet
 
     private lateinit var customWeekView: CustomWeekView
 
     @Before
     fun setup() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        val attrs = mock(AttributeSet::class.java)
-        customWeekView = CustomWeekView(context, attrs)
+        MockitoAnnotations.initMocks(this)
+
+        `when`(WeekView(any(), any())).thenReturn(null)
+        `when`(mockContext.theme.obtainStyledAttributes(any(), any(), anyInt(), anyInt())).thenReturn(null)
+//        `when`(mockContext.resources).thenReturn(mockResources)
+//        `when`(mockResources.displayMetrics).thenReturn(mock(DisplayMetrics::class.java))
+//        `when`(mockConfiguration.isLayoutSizeAtLeast(anyInt())).thenReturn(true)
+
+        customWeekView = CustomWeekView(mockContext, mockAttributeSet)
+    }
+
+    @Test
+    fun onTouchEvent_downAction_setsStartX() {
+        // Simulate MotionEvent.ACTION_DOWN event
+        val mockEvent = mock(MotionEvent::class.java)
+        `when`(mockEvent.action).thenReturn(MotionEvent.ACTION_DOWN)
+        `when`(mockEvent.x).thenReturn(10f)
+
+        customWeekView.onTouchEvent(mockEvent)
     }
 
     @Test
     fun onTouchEvent_moveAction_ignoreHorizontalScrolling() {
-        val distanceX = 20f
-        val event = MotionEvent.obtain(0L, 0L, MotionEvent.ACTION_MOVE, distanceX, 0f, 0)
-        val result = customWeekView.onTouchEvent(event)
+        // Simulate MotionEvent.ACTION_MOVE event
+        val mockEvent = mock(MotionEvent::class.java)
+        `when`(mockEvent.action).thenReturn(MotionEvent.ACTION_MOVE)
+        `when`(mockEvent.x).thenReturn(30f)
 
+        val result = customWeekView.onTouchEvent(mockEvent)
+
+        // Verify that horizontal scrolling is ignored and returns false
         assertEquals(false, result)
     }
 
     @Test
     fun onTouchEvent_otherActions_delegateToSuper() {
-        val event = MotionEvent.obtain(0L, 0L, MotionEvent.ACTION_UP, 0f, 0f, 0)
-        val result = customWeekView.onTouchEvent(event)
+        // Simulate MotionEvent.ACTION_UP event
+        val mockEvent = mock(MotionEvent::class.java)
+        `when`(mockEvent.action).thenReturn(MotionEvent.ACTION_UP)
 
+        val result = customWeekView.onTouchEvent(mockEvent)
+
+        // Verify that other actions delegate to super and return true
         assertEquals(true, result)
     }
 }
