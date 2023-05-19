359519d050b88843a07c3d35eb039f7b87f277c6
AnirudhhRamesh <anirudhh.ramesh@gmail.com>
2023-05-19 08:36:00 +0200

    WIP Setting up coaching offers screen

--- app/src/main/java/com/github/mateo762/myapplication/coaching/fragments/OffersFragment.kt
+++ app/src/main/java/com/github/mateo762/myapplication/coaching/fragments/OffersFragment.kt
@@ -1,16 +1,34 @@
 package com.github.mateo762.myapplication.coaching.fragments
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.lifecycleScope
+import com.github.mateo762.myapplication.models.HabitEntity
+import com.github.mateo762.myapplication.models.UserEntity
 import com.github.mateo762.myapplication.ui.coaching.OffersScreen
-import dagger.hilt.android.AndroidEntryPoint
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import kotlinx.coroutines.launch
 
 class OffersFragment : Fragment() {
 
+    //Reference to the habits database
+    private lateinit var habitsRef: DatabaseReference
+
+    //Create an empty habit list state
+    val habitsState = mutableStateOf(emptyList<HabitEntity>())
+
+    // A list of habits where coaching has been requested
+    private val coachingRequested =
+        mutableStateOf(mutableListOf<Map<HabitEntity, UserEntity>>())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,10 +36,96 @@ class OffersFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                // TODO: Arnie
-                OffersScreen()
+                OffersScreen(coachingRequested.value) { coachee, habit ->
+                    habitsRef.orderByChild("id").equalTo(habit.id)
+                        .addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                snapshot.children.forEach { childSnapshot ->
+
+                                    //Add the coach to the list of coaches for the habit
+                                }
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {}
+                        })
+                }
             }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Get the connectivityManager to verify network functions
+        val connectivityManager =
+            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        // Verify we have connection -- this way we will at least always run the Listener,
+        // and if Firebase fails, then we run the failed action onCancelled
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        val connectionExists =
+            networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+        // Connect to Firebase for real time data
+        if (connectionExists) {
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            if (currentUser != null) {
+                val habitsPath = "/users/${currentUser.uid}/habitsPath"
+                val coachingHabits = "coachingHabits"
+
+                //TODO: Create a "coachable habits" public path on firebase and refactor the habit storing methods
+
+                lifecycleScope.launch {
+                    getFirebaseHabits(habitsPath)
+                }
+            }
+        }
+    }
+
+    private fun getCurrentUser(): UserEntity{
+        return UserEntity("currentUserId", "getCurrentUser", "coachee_user", "coachee_user@email.com")
+    }
+
+    suspend fun getCoachableHabits() {
+        val coachableHabitsState: MutableList<Map<HabitEntity, UserEntity>> = mutableListOf()
+        for (coachableHabit in habitsState.value) {
+            //Add all habits where coaching is requested and the habit is not yet coached
+            if (coachableHabit.coachRequested && !coachableHabit.isCoached) {
+                coachableHabitsState.add(mapOf(coachableHabit to getCurrentUser()))
+            }
+            this@OffersFragment.coachingRequested.value = coachableHabitsState
+        }
+    }
+
+    //Retrieve the list of all habits from firebase
+    private fun getFirebaseHabits(path: String) {
+        // Initialize Firebase database reference
+        habitsRef = FirebaseDatabase.getInstance().getReference(path)
+        habitsRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val coachableHabits = mutableListOf<HabitEntity>()
+                for (childSnapshot in snapshot.children) {
+                    val habit = childSnapshot.getValue(HabitEntity::class.java)
+                    // BUG? snapshot.getValue forces isCoached to false, but not the other values?
+                    if (habit != null && habit.coachRequested) {
+                        if (childSnapshot.hasChild("isCoached")) {
+                            // If the snapshot contains a value for isCoached, use it
+                            habit.isCoached =
+                                childSnapshot.child("isCoached").getValue(Boolean::class.java)
+                                    ?: false
+                            // Otherwise, set it to false
+                        } else {
+                            habit.isCoached = false
+                        }
+                        coachableHabits.add(habit)
+                    }
+                }
+                lifecycleScope.launch {
+                    habitsState.value = coachableHabits
+                    getCoachableHabits()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
     
 }
--- app/src/main/java/com/github/mateo762/myapplication/ui/coaching/OffersScreen.kt
+++ app/src/main/java/com/github/mateo762/myapplication/ui/coaching/OffersScreen.kt
@@ -1,35 +1,190 @@
 package com.github.mateo762.myapplication.ui.coaching
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.github.mateo762.myapplication.R
+import com.github.mateo762.myapplication.models.HabitEntity
+import com.github.mateo762.myapplication.models.UserEntity
 
 
 @Composable
-fun OffersScreen() {
+fun OffersScreen(
+    coachingRequested: List<Map<HabitEntity, UserEntity>>,
+    onCoachingOffered: (UserEntity, HabitEntity) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .background(colorResource(R.color.white))
+    ) {
+        if (coachingRequested.isEmpty()) {
+            DisplayNoOffers()
+        } else {
+            LazyColumn {
+                items(coachingRequested) { habit ->
+                    //Display each habit where coaching is requested
+
+                    DisplayCoachingOffers(habit) { itCoach, itHabit ->
+                        onCoachingOffered(
+                            itCoach,
+                            itHabit
+                        ) // Invoke the callback with coach and habit
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DisplayNoOffers() {
     Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .background(colorResource(R.color.card_background_light), RoundedCornerShape(8.dp))
-            .padding(vertical = 16.dp, horizontal = 16.dp)
-    ) {
-        Column {
-            Text(
-                text = "TODO: Offers",
-                style = MaterialTheme.typography.h1,
-                modifier = Modifier.testTag("placeholder_text")
+            .background(
+                colorResource(R.color.white),
+                RoundedCornerShape(8.dp)
             )
+            .fillMaxWidth()
+            .padding(vertical = 16.dp, horizontal = 16.dp)
+            .testTag("nothing_to_see_box")
+    ) {
+        Text(
+            text = "There are no requests for coaching",
+            style = MaterialTheme.typography.h6,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier.testTag("nothing_to_see_text")
+
+        )
+    }
+}
+
+@Composable
+fun DisplayCoachingOffers(
+    habitMap: Map<HabitEntity, UserEntity>,
+    OnCoachingOffered: (UserEntity, HabitEntity) -> Unit // Callback for coach selection
+) {
+    for (habit in habitMap.keys) {
+        Box(
+            modifier = Modifier
+                .background(
+                    colorResource(R.color.card_background_light),
+                    RoundedCornerShape(8.dp)
+                )
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 16.dp)
+                //.size(400.dp, 300.dp)
+                .testTag("habit_selection_box")
+
+        ) {
+            Column {
+                Text(
+                    text = habit.name,
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.testTag("habit_name_${habit.name}")
+                )
+                Row(modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)) {
+                    var coachee = habitMap[habit]!!
+                    CoacheeCard(name = coachee.name!!, username = coachee.username!!, email = coachee.email!!) {
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier
+            .height(16.dp)
+            .testTag("habit_spacer"))
+    }
+}
+
+
+@Composable
+fun CoacheeCard(
+    name: String, username: String, email: String,
+    onSelected: () -> Unit
+) {
+    LocalContext.current
+    Row(modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_android),
+            contentDescription = "avatar_$username",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(37.dp)
+                .clip(CircleShape)
+                .border(
+                    1.dp,
+                    colorResource(R.color.today_background),
+                    CircleShape
+                )
+                .align(Alignment.CenterVertically)
+                .testTag("candidate_card_image")
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.testTag("candidate_card_name_$name"),
+            )
+            Text(
+                text = username,
+                style = MaterialTheme.typography.body1,
+                fontStyle = FontStyle.Italic,
+                modifier = Modifier.testTag("candidate_card_username_$username"),
+            )
+            Text(
+                text = email,
+                style = MaterialTheme.typography.body1,
+                fontStyle = FontStyle.Italic,
+                modifier = Modifier.testTag("candidate_card_email_$email"),
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_star_24),
+                    contentDescription = "star_icon",
+                    modifier = Modifier
+                        .size(23.dp)
+                        .align(Alignment.CenterVertically)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = {
+                onSelected()
+            },
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .testTag("candidate_card_button"),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = colorResource(id = R.color.today_background),
+                contentColor = colorResource(R.color.white)
+            )
+        ) {
+            Text(text = "Select")
         }
     }
 }
