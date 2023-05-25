8cf7fafbf05f3735d6853340932b35c2a6459a31
Mateo Echeverry <mateo.echeverryhoyos@gmail.com>
2023-05-25 16:34:44 +0200

    Trying to fix bugs

--- app/src/androidTest/java/com/github/mateo762/myapplication/feed/FeedFragmentTestt.kt
+++ app/src/androidTest/java/com/github/mateo762/myapplication/feed/FeedFragmentTestt.kt
@@ -1,65 +1,81 @@
 package com.github.mateo762.myapplication.feed
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.fragment.NavHostFragment
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.mateo762.myapplication.R
+import com.github.mateo762.myapplication.home.HomeActivity
 import com.github.mateo762.myapplication.home.fragments.FeedFragment
-import com.github.mateo762.myapplication.models.HabitEntity
-import com.github.mateo762.myapplication.models.HabitImageEntity
 import com.github.mateo762.myapplication.models.PostEntity
-import com.github.mateo762.myapplication.models.UserEntity
 import com.github.mateo762.myapplication.room.PostDao
 import com.github.mateo762.myapplication.room.PostRepository
-import com.github.mateo762.myapplication.ui.home.FeedScreen
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.*
-import org.mockito.MockitoAnnotations
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
 class FeedFragmentTestt {
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    private lateinit var feedFragment: FeedFragment
 
     @Before
-    fun setUp() {
-        //MockitoAnnotations.openMocks(this)
-        composeTestRule.setContent {
-            FeedScreen(posts = emptyList())
+    fun setup() {
+        hiltRule.inject()
+        feedFragment = FeedFragment()
+        // Mock the PostRepository
+        val dummyPostDao = object : PostDao {
+            override fun getAll(): List<PostEntity> {
+                return listOf()
+            }
+
+            override fun insertAll(posts: List<PostEntity>) {
+                return
+            }
+
+            override fun delete(post: PostEntity) {
+                return
+            }
+
         }
+        feedFragment.postRepository = PostRepository(dummyPostDao)
     }
 
     @Test
-    fun testFeed_fetchFollowingUsers() {
-/*
-        val user1 = UserEntity("user1", "james", "james2", "james@gmail.com", emptyList(), emptyList(),
-        emptyList(), emptyList()
-        )
-        val user2 = UserEntity("user2", "bob", "bob3", "bob@gmail.com", emptyList(), emptyList(),
-        emptyList(), emptyList()
-        )
-
-        val mockDataSnapshot = mock(DataSnapshot::class.java)
-        `when`(mockDataSnapshot.value).thenReturn(
-            user1
-        )
-        val mockUserDatabaseReference = mock(DatabaseReference::class.java)
-        `when`(mockDataSnapshot.ref).thenReturn(mockUserDatabaseReference)
-
-*/
-
-        val feedFragment = FeedFragment()
-        feedFragment.fetchFollowingUsers(
-            FirebaseDatabase.getInstance().getReference("/users/mTFQAS8YmlXK89siWb36PwIe1x82"))
+    fun fetchFollowingUsersTest() {
+        val firebaseDatabase = FirebaseDatabase.getInstance()
+        val userReference = firebaseDatabase.getReference("dummy")
+        feedFragment.fetchFollowingUsers(userReference)
     }
 
+    @Test
+    fun fetchUserImagesTest() {
+        feedFragment.fetchUserImages("dummyId")
+    }
+
+    @Test
+    fun generatePostsTest() {
+        feedFragment.generatePosts(listOf())
+    }
+
+    @Test
+    fun updatePostsCacheTest() {
+        feedFragment.updatePostsCache(listOf())
+    }
+
+    @Test
+    fun getLocalPostsTest() {
+        feedFragment.getLocalPosts()
+    }
 }
