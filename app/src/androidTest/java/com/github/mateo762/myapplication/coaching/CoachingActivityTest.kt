134dc34c4acf184e81380a3b387bbb1f30291a95
AnirudhhRamesh <anirudhh.ramesh@gmail.com>
2023-05-27 00:41:37 +0200

    Merge coaching activity test

--- app/src/androidTest/java/com/github/mateo762/myapplication/coaching/CoachingActivityTest.kt
+++ app/src/androidTest/java/com/github/mateo762/myapplication/coaching/CoachingActivityTest.kt
@@ -2,9 +2,12 @@ package com.github.mateo762.myapplication.coaching
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+<<<<<<< HEAD
 import com.github.mateo762.myapplication.ui.coaching.OffersScreen
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+=======
+>>>>>>> parent of 19d5828 (Fixed CaochingActivityTest failing)
 import junit.framework.TestCase.assertNotNull
 import org.junit.Before
 import org.junit.Rule
@@ -13,19 +16,17 @@ import org.junit.runner.RunWith
 import org.mockito.MockitoAnnotations
 
 @RunWith(AndroidJUnit4::class)
-@HiltAndroidTest
 class CoachingActivityTest {
 
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
 
-    @get:Rule
-    val activityScenarioRule = ActivityScenarioRule(CoachingActivity.CoachingEntryPoint::class.java)
-
     @Before
     fun setup() {
         hiltRule.inject()
     }
+
+    val activityScenarioRule = ActivityScenarioRule(CoachingActivity::class.java)
 
     @Test
     fun testActivityLaunchesSuccessfully() {
